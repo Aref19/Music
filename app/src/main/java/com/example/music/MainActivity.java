eef5f5a40156eb52224be64d5d3aa3c21d5304f0
aref19 <arefobaid6@gmail.com>
2020-05-11 17:04:32 +0200

    music

--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.music;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -12,11 +13,16 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.Toast;
@@ -26,11 +32,16 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     SeekBar seekBar;
-    MediaPlayer mediaPlayer=new MediaPlayer();
+    MediaPlayer mediaPlayer = new MediaPlayer();
     Cursor crusor;
     ArrayList<Songinfo> songinfos;
     ListView listView;
-    int prmistion=1,postion;
+    int prmistion = 1, postion, prograss;
+    Button  start, stop;
+    Runnable runnable, runn;
+    Handler handler, handel;
+    boolean sorstop;
+    ImageView bause;
 
 
     @Override
@@ -38,49 +49,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         premtion();
-        seekBar=findViewById(R.id.laufm);
-        listView=findViewById(R.id.liedlist);
-      getallsong();
-      sekk(seekBar);
+        seekBar = findViewById(R.id.laufm);
+        listView = findViewById(R.id.liedlist);
+        handler = new Handler();
+        handel = new Handler();
+        getallsong();
+        sekk(seekBar);
+        bause = findViewById(R.id.pause);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+        buttonClick();
+        current();
 
 
     }
-    private  void getallsong(){
-        songinfos=new ArrayList<>();
-        Uri allsong= MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        String selection=MediaStore.Audio.Media.IS_MUSIC+"!=0";
-        crusor=managedQuery(allsong,null,selection,null,null);
-        if(crusor!=null){
-            if (crusor.moveToNext()){
-                do{
-                    String name=crusor.getString(crusor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
-                    String fullp=crusor.getString(crusor.getColumnIndex(MediaStore.Audio.Media.DATA));
-                    String alpum=crusor.getString(crusor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
-                    String artist=crusor.getString(crusor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
-Toast.makeText(this,"sda"+name,Toast.LENGTH_LONG).show();
-                 songinfos.add(new Songinfo(fullp,name,alpum,artist));
-                }while (crusor.moveToNext());
-                Toast.makeText(this,"sdaaaaaaa"+songinfos.get(0).getSong_name(),Toast.LENGTH_LONG).show();
-                Adabter adabter=new Adabter(songinfos,this);
+
+    private void getallsong() {
+        songinfos = new ArrayList<>();
+        Uri allsong = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC + "!=0";
+        crusor = managedQuery(allsong, null, selection, null, null);
+        if (crusor != null) {
+            if (crusor.moveToNext()) {
+                do {
+                    String name = crusor.getString(crusor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+                    String fullp = crusor.getString(crusor.getColumnIndex(MediaStore.Audio.Media.DATA));
+                    String alpum = crusor.getString(crusor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                    String artist = crusor.getString(crusor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                    Log.i("name", "getallsong: " + name.toString());
+                    if (!name.contains("WA")) {
+
+
+                        songinfos.add(new Songinfo(fullp, name, alpum, artist));
+
+                    }
+
+                } while (crusor.moveToNext());
+                //       Toast.makeText(this,"sdaaaaaaa"+songinfos.get(0).getSong_name(),Toast.LENGTH_LONG).show();
+                Adabter adabter = new Adabter(songinfos, this);
 
                 listView.setAdapter(adabter);
-                list(listView,songinfos);
+                list(listView, songinfos);
             }
             crusor.close();
         }
     }
-    private void list(ListView listView, final ArrayList<Songinfo> songinfos){
+
+    private void list(ListView listView, final ArrayList<Songinfo> songinfos) {
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 try {
+                    prograss = position;
                     mediaPlayer.stop();
-                    mediaPlayer=new MediaPlayer();
+                    mediaPlayer = new MediaPlayer();
                     mediaPlayer.setDataSource(songinfos.get(position).getPath());
                     mediaPlayer.prepare();
                     mediaPlayer.start();
+                    bause.setImageResource(R.drawable.start);
+
                     seekBar.setMax(mediaPlayer.getDuration());
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -88,23 +117,25 @@ Toast.makeText(this,"sda"+name,Toast.LENGTH_LONG).show();
             }
         });
     }
-    private  void premtion(){
-        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
-            Toast.makeText(this,"you have",Toast.LENGTH_LONG).show();
-        }else{
+
+    private void premtion() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+        } else {
             requstpremstion();
         }
 
-}
-private void requstpremstion(){
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+    }
+
+    private void requstpremstion() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
             new AlertDialog.Builder(this)
                     .setTitle("premision")
                     .setMessage("dsa")
                     .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},prmistion);
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, prmistion);
 
                         }
                     }).setNegativeButton("no", new DialogInterface.OnClickListener() {
@@ -114,17 +145,22 @@ private void requstpremstion(){
                 }
             }).create().show();
 
-        }else {
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},prmistion);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, prmistion);
         }
     }
-    private void sekk(SeekBar seekBar){
+
+    private void sekk(SeekBar seekBar) {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if(fromUser){
-                    postion=progress;
-                    mediaPlayer.seekTo(postion);
+                if (fromUser) {
+                    postion = progress;
+                    mediaPlayer.seekTo(progress);
+
+                } else if (progress == mediaPlayer.getDuration()) {
+                    media();
+
                 }
 
             }
@@ -138,7 +174,80 @@ private void requstpremstion(){
             public void onStopTrackingTouch(SeekBar seekBar) {
                 mediaPlayer.seekTo(postion);
 
+                ;
+
+
             }
         });
+    }
+
+    private void buttonClick() {
+        bause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+                if(sorstop){
+                    bause.setImageResource(R.drawable.start);
+                }else{
+                    bause.setImageResource(R.drawable.stop);
+                }
+
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                mediaPlayer=new MediaPlayer();
+                bause.setImageResource(R.drawable.stop);
+                seekBar.setProgress(0);
+            }
+        });
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+                bause.setImageResource(R.drawable.start);
+            }
+        });
+
+    }
+
+    private void current() {
+        seekBar.setProgress(mediaPlayer.getCurrentPosition());
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                current();
+            }
+        };
+        handler.postDelayed(runnable, 1000);
+
+    }
+
+    private void media() {
+        try {
+            prograss += 1;
+            if(prograss>=songinfos.size()){
+                prograss=0;
+            }
+            mediaPlayer.stop();
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(songinfos.get(prograss).getPath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            seekBar.setMax(mediaPlayer.getDuration());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mediaPlayer=new MediaPlayer();
     }
 }
