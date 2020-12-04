6dd3be0d91ee2b5600a64c217582871db7212f8e
aref19 <arefobaid6@gmail.com>
2020-12-04 19:51:34 +0100

    jetzt

--- app/src/main/java/com/example/music/HauptMain/Main.java
+++ app/src/main/java/com/example/music/HauptMain/Main.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -20,25 +22,41 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         BottomNavigationView bottomNavigationView=findViewById(R.id.navibut);
-        bottomNavigationView.setOnNavigationItemReselectedListener(nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(nav);
         getSupportFragmentManager().beginTransaction().replace(R.id.fram,new MusicNavie()).commit();
     }
-    private  BottomNavigationView.OnNavigationItemReselectedListener nav=new BottomNavigationView.OnNavigationItemReselectedListener() {
+    private  BottomNavigationView.OnNavigationItemSelectedListener nav=new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
-        public void onNavigationItemReselected(@NonNull MenuItem item) {
-            Fragment fragment=null;
-            switch (item.getItemId()){
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
                 case R.id.music:
-                    fragment=new MusicNavie();
-                    Log.i("frag", "onNavigationItemReselected: "+"fra");
+                    fragment = new MusicNavie();
+                    Log.i("frag", "onNavigationItemReselected: " + "fra");
                     break;
                 case R.id.video:
-                    fragment=new VideoFragment();
+                    fragment = new VideoFragment();
                     break;
 
-
             }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fram,fragment).commit();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fram, fragment).commit();
+            return true;
         }
     };
+
+    @Override
+    public void onBackPressed() {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Main.super.onBackPressed();
+                    }
+                }).create().show();
+    }
 }
